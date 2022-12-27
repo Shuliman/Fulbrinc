00093b62ff965a6853bffe4efcc62a8fec11426d
Shu <email@address.com>
2022-12-27 14:58:18 -0500

    Fixed back-end, posts is work

--- fulbrinc-backend/app/Models/User.php
+++ fulbrinc-backend/app/Models/User.php
@@ -11,17 +11,11 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasApiTokens;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -31,7 +25,6 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-
     /**
      * The attributes that should be cast to native types.
      *
@@ -40,4 +33,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
