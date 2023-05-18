4d0afea21de2ae55482769fcab45e7ac444bfee5
Shuliman <Julik.schuldiner@proton.me>
2023-05-18 17:47:47 +0200

    Deleted unnecessary component and switched button from home to bookmarks

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,5 +1,4 @@
 import { createWebHistory, createRouter } from "vue-router";
-import MainHome from "./components/MainHome.vue";
 import LoginUser from "./components/LoginUser.vue";
 import RegistrationUser from "./components/RegistrationUser.vue";
 import UserProfile from "./components/UserProfile.vue";
@@ -8,15 +7,6 @@ import BoardModerator from "./components/BoardModerator.vue";
 import BoardBookmark from "./components/BoardBookmark.vue";
 
 const routes = [{
-        path: "/",
-        name: "home",
-        component: MainHome,
-    },
-    {
-        path: "/home",
-        component: MainHome,
-    },
-    {
         path: "/login",
         name: "login",
         component: LoginUser,
