5fad8c42962e063178661ad8f982f61697725278
Shuliman <Julik.schuldiner@proton.me>
2023-06-01 15:02:52 +0200

    Added method in Hadler for invalid user data error processing

--- back-end/app/Exceptions/Handler.php
+++ back-end/app/Exceptions/Handler.php
@@ -4,6 +4,8 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -47,4 +49,12 @@ class Handler extends ExceptionHandler
             //
         });
     }
+    public function render($request, Throwable $exception)
+{
+    if ($exception instanceof ValidationException) {
+        return response()->json(['message' => 'Invalid user data'], 400);
+    }
+
+    return parent::render($request, $exception);
+}
 }
