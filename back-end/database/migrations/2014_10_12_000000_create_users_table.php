74c6660f239db1530fdd2b1f8add275999e875bf
Shuliman <Julik.schuldiner@proton.me>
2023-06-07 12:31:02 +0200

    Deleted unnecessary email_verificated column from DB

--- back-end/database/migrations/2014_10_12_000000_create_users_table.php
+++ back-end/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('name');
             $table->boolean('is_admin')->default(false);
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
