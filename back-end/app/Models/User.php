073d5a8e56c3f7e5a965d64cdeaaaed18b6f84f0
Shuliman <Julik.schuldiner@proton.me>
2023-06-12 18:27:34 +0200

    Deleted unnecessary email verificated column from User model

--- back-end/app/Models/User.php
+++ back-end/app/Models/User.php
@@ -37,7 +37,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
         'is_admin' => 'boolean',
     ];
     public function posts()
