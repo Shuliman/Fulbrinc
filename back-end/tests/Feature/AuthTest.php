ccb9c63b1973fada75f482722e6e27d5865ff68b
Shuliman <email@address.com>
2023-06-29 11:26:32 -0400

    Updated Authtest

--- back-end/tests/Feature/AuthTest.php
+++ back-end/tests/Feature/AuthTest.php
@@ -3,15 +3,15 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
 use Faker\Factory as Faker;
+use Tests\TestCase;
 use App\Models\User;
 
 class AuthTest extends TestCase
 {
     //use RefreshDatabase;
 
+    private $user;
     private $faker;
     private $email;
     private $pass;
@@ -21,6 +21,7 @@ class AuthTest extends TestCase
     {
         parent::setUp();
 
+        $this->user = User::factory()->create();
         $this->faker = Faker::create();
         $this->email = $this->faker->email;
         $this->pass = $this->faker->password(8,16);
@@ -52,37 +53,32 @@ class AuthTest extends TestCase
         $this->assertEquals(200,$response->status());
 
     }
+
     public function test_invalid_login()
     {
         $response = $this->post('/api/login',[
-        'email' => $this->faker->email,
-        'password' => $this->pass,
+        'email' => 'fakeemail@example.com',
+        'password' => 'password',
         ]);
         $this->assertEquals(401, $response->status());
     }
 
-  public function test_invalid_password()
-  {
-    User::create([
-      'name' => $this->name,
-      'email' => $this->email,
-      'password' => bcrypt($this->pass),
-    ]);
+    public function test_invalid_password()
+    {
+        $response = $this->post('/api/login',[
+            'email' => $this->user->email,
+            'password' => 'wrongpassword', // Используем неверный пароль
+        ]);
+        $this->assertEquals(401, $response->status());
+    }
 
-    $response = $this->post('/api/login',[
-      'email' => $this->email,
-      'password' => $this->faker->password, //using two different passwords
-    ]);
-    $this->assertEquals(401, $response->status());
-  }
-
-  public function test_invalid_email()
-  {
-    $response = $this->post('/api/register',[
-      'name' => $this->name,
-      'email' => 'invalid email',
-      'password' => $this->pass,
-    ]);
-    $this->assertEquals(400, $response->status());
-  }
+    public function test_invalid_email()
+    {
+        $response = $this->post('/api/register',[
+            'name' => $this->user->name,
+            'email' => 'invalid email',
+            'password' => 'password',
+        ]);
+        $this->assertEquals(400, $response->status());
+    }
 }
