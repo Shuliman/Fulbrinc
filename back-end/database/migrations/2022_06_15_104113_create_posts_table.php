af3e8d666783b967a8bbd532eda9c0453ad2de1c
Shuliman <email@address.com>
2023-07-04 06:48:15 -0400

    Implemented cascade deletion of posts when a user is deleted and resolved a bug

--- back-end/database/migrations/2022_06_15_104113_create_posts_table.php
+++ back-end/database/migrations/2022_06_15_104113_create_posts_table.php
@@ -19,7 +19,7 @@ class CreatePostsTable extends Migration
             $table->text('title');
             $table->longText('description');
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
