35b898e60b7ce033fbfc404d4dd5fa33691234c3
Shuliman <email@address.com>
2023-07-14 06:13:33 -0400

    Unwork

--- front-end/src/components/UserSettings.vue
+++ front-end/src/components/UserSettings.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="user-settings">
       <h1>User Settings</h1>
-      
       <form @submit.prevent="updateSettings">
         <label for="username">Username:</label>
         <input id="username" v-model="settings.username" type="text" />
   
         <label for="email">Email:</label>
         <input id="email" v-model="settings.email" type="email" />
-  
+
+        <label for="old_password">Old Password:</label>
+        <input id="old_password" v-model="settings.old_password" type="password" />
+        
+        <label for="password">Password:</label>
+        <input id="password" v-model="settings.password" type="password" />
         <!-- Add other settings fields here -->
   
         <button type="submit">Save Changes</button>
       </form>
-  
       <p v-if="message">{{ message }}</p>
     </div>
   </template>
@@ -21,8 +24,9 @@
   <script>
   import axios from 'axios';
   
+  const accessToken = localStorage.getItem('accessToken'); // getting access token from localStorage
   const API_URL = 'http://127.0.0.1:8080/api';
-
+  
   export default {
     data() {
       return {
@@ -37,26 +41,41 @@
     methods: {
       async updateSettings() {
         try {
-          await axios.put(API_URL +'/settings', this.settings, {
+          let formData = new FormData();
+          formData.append('username', this.settings.username);
+          formData.append('email', this.settings.email);
+          formData.append('old_password', this.settings.old_password);
+          formData.append('password', this.settings.password);
+          // append other settings here
+  
+          await axios({
+            method: 'put',
+            url: API_URL + '/settings',
+            data: formData,
             headers: {
-              'Authorization': `Bearer ${this.$store.state.token}`
+              'Authorization': 'Bearer ' + accessToken,
+              'Content-Type': 'application/x-www-form-urlencoded'
             }
           });
           this.message = 'Settings updated successfully!';
         } catch (error) {
+          console.error('Error updating settings:', error); // log the error to the console
           this.message = 'An error occurred while updating settings.';
         }
       },
     },
     async created() {
       try {
-        const response = await axios.get(API_URL +'/settings', {
+        const response = await axios({
+          method: 'get',
+          url: API_URL + '/settings',
           headers: {
-            'Authorization': `Bearer ${this.$store.state.token}`
+            'Authorization': 'Bearer ' + accessToken,
           }
         });
         this.settings = response.data;
       } catch (error) {
+        console.error('Error fetching settings:', error); // log the error to the console
         this.message = 'An error occurred while fetching settings.';
       }
     },
